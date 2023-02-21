50973069f558584002c491239bab2ac89bb0ff6a
Kyle Williams <kwilliamsdev98@gmail.com>
2023-02-21 16:46:52 -0500

    Test 20

--- src/components/ContactModal/ContactModal.jsx
+++ src/components/ContactModal/ContactModal.jsx
@@ -40,20 +40,6 @@ export const ContactModal = () => {
             </label>
           </p>
           <p>
-            <label>
-              Your Role:{" "}
-              <select name="role[]" multiple>
-                <option value="leader">Leader</option>
-                <option value="follower">Follower</option>
-              </select>
-            </label>
-          </p>
-          <p>
-            <label>
-              Message: <textarea name="message"></textarea>
-            </label>
-          </p>
-          <p>
             <button type="submit">Send</button>
           </p>
         </form>
